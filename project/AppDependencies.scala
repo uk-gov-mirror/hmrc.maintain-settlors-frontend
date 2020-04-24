1092482e8802c1c26bef25510470f02b185cabab
Adam Conder <adamdconder@gmail.com>
2020-04-24 11:04:36 +0100

    Disabling junit plugin that reports parsing error

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,24 +6,24 @@ object AppDependencies {
   val compile = Seq(
     play.sbt.PlayImport.ws,
     "org.reactivemongo" %% "play2-reactivemongo"            % "0.18.6-play26",
-    "uk.gov.hmrc"       %% "logback-json-logger"            % "4.6.0",
+    "uk.gov.hmrc"       %% "logback-json-logger"            % "4.8.0",
     "uk.gov.hmrc"       %% "govuk-template"                 % "5.52.0-play-26",
     "uk.gov.hmrc"       %% "play-health"                    % "3.14.0-play-26",
     "com.typesafe.play" %% "play-json-joda"                 % "2.7.4",
     "uk.gov.hmrc"       %% "play-ui"                        % "8.8.0-play-26",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.2.0-play-26",
-    "uk.gov.hmrc"       %% "bootstrap-play-26"              % "1.4.0",
+    "uk.gov.hmrc"       %% "bootstrap-play-26"              % "1.6.0",
     "uk.gov.hmrc"       %% "play-whitelist-filter"          % "3.1.0-play-26"
   )
 
   val test = Seq(
-    "org.scalatest"               %% "scalatest"          % "3.0.7",
+    "org.scalatest"               %% "scalatest"          % "3.0.8",
     "org.scalatestplus.play"      %% "scalatestplus-play" % "3.1.2",
     "org.pegdown"                 %  "pegdown"            % "1.6.0",
-    "org.jsoup"                   %  "jsoup"              % "1.10.3",
+    "org.jsoup"                   %  "jsoup"              % "1.12.1",
     "com.typesafe.play"           %% "play-test"          % PlayVersion.current,
     "org.mockito"                 %  "mockito-all"        % "1.10.19",
-    "org.scalacheck"              %% "scalacheck"         % "1.14.0"
+    "org.scalacheck"              %% "scalacheck"         % "1.14.1"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
