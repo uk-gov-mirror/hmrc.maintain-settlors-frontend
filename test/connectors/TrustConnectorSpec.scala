383ea36459b6adbc9628db00d84949ce1eb9bfec
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2020-04-27 14:33:08 +0100

    Add to page specs.

---
+++ test/connectors/TrustConnectorSpec.scala
@@ -0,0 +1,235 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors
+
+import java.time.LocalDate
+
+import base.SpecBase
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock.{get, okJson, urlEqualTo}
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig
+import generators.Generators
+import models.settlors.{BusinessSettlor, IndividualSettlor, Settlors}
+import models.{CompanyType, Name, TrustDetails, TypeOfTrust}
+import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, Inside}
+import play.api.libs.json.Json
+import uk.gov.hmrc.http.HeaderCarrier
+
+class TrustConnectorSpec extends SpecBase with Generators with ScalaFutures
+  with Inside with BeforeAndAfterAll with BeforeAndAfterEach with IntegrationPatience {
+  implicit lazy val hc: HeaderCarrier = HeaderCarrier()
+
+  protected val server: WireMockServer = new WireMockServer(wireMockConfig().dynamicPort())
+
+  override def beforeAll(): Unit = {
+    server.start()
+    super.beforeAll()
+  }
+
+  override def beforeEach(): Unit = {
+    server.resetAll()
+    super.beforeEach()
+  }
+
+  override def afterAll(): Unit = {
+    super.afterAll()
+    server.stop()
+  }
+
+  val utr = "1000000008"
+  val index = 0
+  val description = "description"
+  val date: LocalDate = LocalDate.parse("2019-02-03")
+
+  "trust connector" when {
+
+    "get trusts details" in {
+
+      val utr = "1000000008"
+
+      val json = Json.parse(
+        """
+          |{
+          | "startDate": "1920-03-28",
+          | "lawCountry": "AD",
+          | "administrationCountry": "GB",
+          | "residentialStatus": {
+          |   "uk": {
+          |     "scottishLaw": false,
+          |     "preOffShore": "AD"
+          |   }
+          | },
+          | "typeOfTrust": "Will Trust or Intestacy Trust",
+          | "deedOfVariation": "Previously there was only an absolute interest under the will",
+          | "interVivos": false
+          |}
+          |""".stripMargin)
+
+      val application = applicationBuilder()
+        .configure(
+          Seq(
+            "microservice.services.trusts.port" -> server.port(),
+            "auditing.enabled" -> false
+          ): _*
+        ).build()
+
+      val connector = application.injector.instanceOf[TrustConnector]
+
+      server.stubFor(
+        get(urlEqualTo(s"/trusts/$utr/trust-details"))
+          .willReturn(okJson(json.toString))
+      )
+
+      val processed = connector.getTrustDetails(utr)
+
+      whenReady(processed) {
+        r =>
+          r mustBe TrustDetails(startDate = "1920-03-28", typeOfTrust = TypeOfTrust.WillTrustOrIntestacyTrust)
+      }
+
+    }
+
+    "get settlors returns a trust with empty lists" must {
+
+      "return a default empty list beneficiaries" in {
+
+        val utr = "1000000008"
+
+        val json = Json.parse(
+          """
+            |{
+            | "settlors": {
+            | }
+            |}
+            |""".stripMargin)
+
+        val application = applicationBuilder()
+          .configure(
+            Seq(
+              "microservice.services.trusts.port" -> server.port(),
+              "auditing.enabled" -> false
+            ): _*
+          ).build()
+
+        val connector = application.injector.instanceOf[TrustConnector]
+
+        server.stubFor(
+          get(urlEqualTo(s"/trusts/$utr/transformed/settlors"))
+            .willReturn(okJson(json.toString))
+        )
+
+        val processed = connector.getSettlors(utr)
+
+        whenReady(processed) {
+          result =>
+            result mustBe Settlors(
+              settlor = Nil,
+              settlorCompany = Nil
+            )
+        }
+
+        application.stop()
+      }
+
+    }
+
+    "get settlors" must {
+
+      "parse the response and return the settlors" in {
+        val utr = "1000000008"
+
+        val json = Json.parse(
+          """
+            |{
+            | "settlors" : {
+            |   "settlor" : [
+            |     {
+            |       "lineNo" : "79",
+            |       "name" : {
+            |         "firstName" : "Carmel",
+            |         "lastName" : "Settlor"
+            |       },
+            |       "entityStart" : "2019-09-23"
+            |     }
+            |   ],
+            |   "settlorCompany" : [
+            |     {
+            |       "lineNo" : "110",
+            |       "bpMatchStatus" : "98",
+            |       "name" : "Settlor Org 24",
+            |       "companyType" : "Investment",
+            |       "companyTime" : false,
+            |       "entityStart" : "2019-09-23"
+            |     }
+            |   ]
+            | }
+            |}
+            |""".stripMargin)
+
+        val application = applicationBuilder()
+          .configure(
+            Seq(
+              "microservice.services.trusts.port" -> server.port(),
+              "auditing.enabled" -> false
+            ): _*
+          ).build()
+
+        val connector = application.injector.instanceOf[TrustConnector]
+
+        server.stubFor(
+          get(urlEqualTo(s"/trusts/$utr/transformed/settlors"))
+            .willReturn(okJson(json.toString))
+        )
+
+        val processed = connector.getSettlors(utr)
+
+        whenReady(processed) {
+          result =>
+            result mustBe Settlors(
+              settlor = List(
+                IndividualSettlor(
+                  name = Name("Carmel", None, "Settlor"),
+                  dateOfBirth = None,
+                  identification = None,
+                  address = None,
+                  entityStart = LocalDate.parse("2019-09-23"),
+                  provisional = false
+                )
+              ),
+              settlorCompany = List(
+                BusinessSettlor(
+                  name = "Settlor Org 24",
+                  companyType = Some(CompanyType.Investment),
+                  companyTime = Some(false),
+                  utr = None,
+                  address = None,
+                  entityStart = LocalDate.parse("2019-09-23"),
+                  provisional = false
+                )
+              )
+            )
+        }
+
+        application.stop()
+      }
+
+    }
+
+  }
+
+}
---
+++ test/controllers/AddASettlorControllerSpec.scala
@@ -0,0 +1,348 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import java.time.LocalDate
+
+import base.SpecBase
+import connectors.TrustStoreConnector
+import forms.AddASettlorFormProvider
+import models.settlors.{BusinessSettlor, IndividualSettlor, Settlors}
+import models.{AddASettlor, CompanyType, Name, NationalInsuranceNumber, RemoveSettlor}
+import org.mockito.Matchers.any
+import org.mockito.Mockito.when
+import org.scalatest.concurrent.ScalaFutures
+import play.api.inject.bind
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import services.TrustService
+import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse}
+import utils.AddASettlorViewHelper
+import viewmodels.addAnother.AddRow
+import views.html.{AddASettlorView, MaxedOutSettlorsView}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class AddASettlorControllerSpec extends SpecBase with ScalaFutures {
+
+  lazy val getRoute : String = controllers.routes.AddASettlorController.onPageLoad().url
+  lazy val submitRoute : String = controllers.routes.AddASettlorController.submit().url
+  lazy val submitCompleteRoute : String = controllers.routes.AddASettlorController.submitComplete().url
+
+  val mockStoreConnector : TrustStoreConnector = mock[TrustStoreConnector]
+
+  val addTrusteeForm = new AddASettlorFormProvider()()
+
+  private def individualSettlor(provisional: Boolean) = IndividualSettlor(
+    name = Name(firstName = "First", middleName = None, lastName = "Last"),
+    dateOfBirth = Some(LocalDate.parse("1983-09-24")),
+    identification = Some(NationalInsuranceNumber("JS123456A")),
+    address = None,
+    entityStart = LocalDate.parse("2019-02-28"),
+    provisional = provisional
+  )
+
+  private def businessSettlor(provisional: Boolean) = BusinessSettlor(
+    name = "Humanitarian Company Ltd",
+    companyType = Some(CompanyType.Investment),
+    companyTime = Some(false),
+    utr = None,
+    address = None,
+    entityStart = LocalDate.parse("2012-03-14"),
+    provisional = provisional
+  )
+
+  private val settlors = Settlors(
+    List(individualSettlor(true)),
+    List(businessSettlor(true))
+  )
+
+  lazy val featureNotAvailable : String = controllers.routes.FeatureNotAvailableController.onPageLoad().url
+
+  val settlorRows = List(
+    AddRow("First Last", typeLabel = "Individual settlor", "Change details", Some(featureNotAvailable), "Remove", Some(controllers.individual.living.remove.routes.RemoveIndividualSettlorController.onPageLoad(0).url)),
+    AddRow("Humanitarian Company Ltd", typeLabel = "Business settlor", "Change details", Some(featureNotAvailable), "Remove", Some(controllers.business.remove.routes.RemoveBusinessSettlorController.onPageLoad(0).url))
+  )
+
+  class FakeService(data: Settlors) extends TrustService {
+
+    override def getSettlors(utr: String)(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[Settlors] = Future.successful(data)
+
+    override def getIndividualSettlor(utr: String, index: Int)
+                                         (implicit hc: HeaderCarrier, ex: ExecutionContext): Future[IndividualSettlor] =
+      Future.successful(individualSettlor(true))
+
+    override def getBusinessSettlor(utr: String, index: Int)(implicit hc: HeaderCarrier, ex: ExecutionContext): Future[BusinessSettlor] =
+      Future.successful(businessSettlor(true))
+
+    override def removeSettlor(utr: String, settlor: RemoveSettlor)(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[HttpResponse] =
+      Future.successful(HttpResponse(OK))
+  }
+
+  "AddASettlor Controller" when {
+
+    "no data" must {
+
+      "redirect to Session Expired for a GET if no existing data is found" in {
+
+        val fakeService = new FakeService(Settlors(Nil, Nil))
+
+        val application = applicationBuilder(userAnswers = None).overrides(Seq(
+          bind(classOf[TrustService]).toInstance(fakeService)
+        )).build()
+
+        val request = FakeRequest(GET, getRoute)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual controllers.routes.SessionExpiredController.onPageLoad().url
+
+        application.stop()
+      }
+
+      "redirect to Session Expired for a POST if no existing data is found" in {
+
+        val application = applicationBuilder(userAnswers = None).build()
+
+        val request =
+          FakeRequest(POST, submitRoute)
+            .withFormUrlEncodedBody(("value", AddASettlor.values.head.toString))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+
+        redirectLocation(result).value mustEqual controllers.routes.SessionExpiredController.onPageLoad().url
+
+        application.stop()
+      }
+    }
+
+    "there are settlors" must {
+
+      "return OK and the correct view for a GET" in {
+
+        val fakeService = new FakeService(settlors)
+
+        val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).overrides(Seq(
+          bind(classOf[TrustService]).toInstance(fakeService)
+        )).build()
+
+        val request = FakeRequest(GET, getRoute)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[AddASettlorView]
+
+        status(result) mustEqual OK
+
+        contentAsString(result) mustEqual view(addTrusteeForm, Nil, settlorRows, "The trust has 2 settlors", Nil)(fakeRequest, messages).toString
+
+        application.stop()
+      }
+
+      "redirect to the maintain task list when the user says they are done" in {
+
+        val fakeService = new FakeService(settlors)
+
+        val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).overrides(Seq(
+          bind(classOf[TrustService]).toInstance(fakeService),
+          bind(classOf[TrustStoreConnector]).toInstance(mockStoreConnector)
+        )).build()
+
+        val request =
+          FakeRequest(POST, submitRoute)
+            .withFormUrlEncodedBody(("value", AddASettlor.NoComplete.toString))
+
+        when(mockStoreConnector.setTaskComplete(any())(any(), any())).thenReturn(Future.successful(HttpResponse.apply(200)))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+
+        redirectLocation(result).value mustEqual "http://localhost:9788/maintain-a-trust/overview"
+
+        application.stop()
+      }
+
+      "redirect to the maintain task list when the user says they want to add later" ignore {
+
+        val fakeService = new FakeService(settlors)
+
+        val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).overrides(Seq(
+          bind(classOf[TrustService]).toInstance(fakeService)
+        )).build()
+
+        val request =
+          FakeRequest(POST, submitRoute)
+            .withFormUrlEncodedBody(("value", AddASettlor.YesLater.toString))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+
+        redirectLocation(result).value mustEqual "http://localhost:9788/maintain-a-trust/overview"
+
+        application.stop()
+      }
+
+      "return a Bad Request and errors when invalid data is submitted" in {
+
+        val fakeService = new FakeService(settlors)
+
+        val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).overrides(Seq(
+          bind(classOf[TrustService]).toInstance(fakeService)
+        )).build()
+
+        val request =
+          FakeRequest(POST, submitRoute)
+            .withFormUrlEncodedBody(("value", "invalid value"))
+
+        val boundForm = addTrusteeForm.bind(Map("value" -> "invalid value"))
+
+        val view = application.injector.instanceOf[AddASettlorView]
+
+        val result = route(application, request).value
+
+        status(result) mustEqual BAD_REQUEST
+
+        contentAsString(result) mustEqual view(boundForm, Nil, settlorRows, "The trust has 2 settlors", Nil)(fakeRequest, messages).toString
+
+        application.stop()
+      }
+    }
+
+    "maxed out settlors" must {
+
+      val settlors = Settlors(
+        List.fill(25)(individualSettlor(true)),
+        List.fill(25)(businessSettlor(true))
+      )
+
+      val fakeService = new FakeService(settlors)
+
+      val settlorRows = new AddASettlorViewHelper(settlors).rows
+
+      "return OK and the correct view for a GET" in {
+
+        val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).overrides(Seq(
+          bind(classOf[TrustService]).toInstance(fakeService)
+        )).build()
+
+        val request = FakeRequest(GET, getRoute)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[MaxedOutSettlorsView]
+
+        status(result) mustEqual OK
+
+        val content = contentAsString(result)
+
+        content mustEqual view(settlorRows.inProgress, settlorRows.complete, "The trust has 50 settlors")(fakeRequest, messages).toString
+        content must include("You cannot enter another settlor as you have entered a maximum of 50.")
+        content must include("If you have further settlors to add, write to HMRC with their details.")
+
+        application.stop()
+
+      }
+
+      "return correct view when one type of settlor is maxed out" in {
+
+        val settlors = Settlors(
+          List.fill(25)(individualSettlor(true)),
+          Nil
+        )
+
+        val fakeService = new FakeService(settlors)
+
+        val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).overrides(Seq(
+          bind(classOf[TrustService]).toInstance(fakeService)
+        )).build()
+
+        val request = FakeRequest(GET, getRoute)
+
+        val result = route(application, request).value
+
+        contentAsString(result) must include("You cannot add another individual as you have entered a maximum of 25.")
+        contentAsString(result) must include("If you have further settlors to add within this type, write to HMRC with their details.")
+
+        application.stop()
+
+      }
+
+      "redirect to add to page and set settlors to complete when user clicks continue" in {
+
+        val fakeService = new FakeService(settlors)
+
+        val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).overrides(Seq(
+          bind(classOf[TrustService]).toInstance(fakeService),
+          bind(classOf[TrustStoreConnector]).toInstance(mockStoreConnector)
+        )).build()
+
+        val request = FakeRequest(POST, submitCompleteRoute)
+
+        when(mockStoreConnector.setTaskComplete(any())(any(), any())).thenReturn(Future.successful(HttpResponse.apply(200)))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+
+        redirectLocation(result).value mustEqual "http://localhost:9788/maintain-a-trust/overview"
+
+        application.stop()
+
+      }
+
+    }
+
+    "no provisional settlors" must {
+
+      val settlors = Settlors(
+        List(individualSettlor(false)),
+        List(businessSettlor(false))
+      )
+
+      val settlorRows = List(
+        AddRow("First Last", typeLabel = "Individual settlor", "Change details", Some(featureNotAvailable), "Remove", None),
+        AddRow("Humanitarian Company Ltd", typeLabel = "Business settlor", "Change details", Some(featureNotAvailable), "Remove", None)
+      )
+
+      "return OK and the correct view for a GET with no remove links" in {
+
+        val fakeService = new FakeService(settlors)
+
+        val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).overrides(Seq(
+          bind(classOf[TrustService]).toInstance(fakeService)
+        )).build()
+
+        val request = FakeRequest(GET, getRoute)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[AddASettlorView]
+
+        status(result) mustEqual OK
+
+        contentAsString(result) mustEqual view(addTrusteeForm, Nil, settlorRows, "The trust has 2 settlors", Nil)(fakeRequest, messages).toString
+
+        application.stop()
+      }
+    }
+  }
+}
