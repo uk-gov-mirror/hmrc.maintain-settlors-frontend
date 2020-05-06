12d5975872a035f1b46c89738035117a60d83fd9
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2020-05-06 15:31:09 +0100

    Removed cleanup of deceased base path from user answers to persist additionalSettlorsYesNo value.

--- app/models/UserAnswers.scala
+++ app/models/UserAnswers.scala
@@ -40,7 +40,6 @@ final case class UserAnswers(
     this
       .deleteAtPath(pages.individual.living.basePath)
       .flatMap(_.deleteAtPath(pages.business.basePath))
-      .flatMap(_.deleteAtPath(pages.individual.deceased.basePath))
       .flatMap(_.remove(AddNowPage))
   }
 
