b3b026ae12794f1196db9803360c4a05c5b2abb5
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2020-11-26 18:17:31 +0000

    Controllers, views and routes.

---
+++ app/controllers/individual/living/amend/PassportOrIdCardDetailsController.scala
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.individual.living.amend
+
+import config.annotations.LivingSettlor
+import controllers.actions._
+import controllers.actions.individual.living.NameRequiredAction
+import forms.CombinedPassportOrIdCardDetailsFormProvider
+import javax.inject.Inject
+import models.{CombinedPassportOrIdCard, Mode}
+import navigation.Navigator
+import pages.individual.living.PassportOrIdCardDetailsPage
+import play.api.data.Form
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import repositories.PlaybackRepository
+import uk.gov.hmrc.play.bootstrap.controller.FrontendBaseController
+import utils.countryOptions.CountryOptions
+import views.html.individual.living.amend.PassportOrIdCardDetailsView
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class PassportOrIdCardDetailsController @Inject()(
+                                                   override val messagesApi: MessagesApi,
+                                                   playbackRepository: PlaybackRepository,
+                                                   @LivingSettlor navigator: Navigator,
+                                                   standardActionSets: StandardActionSets,
+                                                   nameAction: NameRequiredAction,
+                                                   formProvider: CombinedPassportOrIdCardDetailsFormProvider,
+                                                   countryOptions: CountryOptions,
+                                                   val controllerComponents: MessagesControllerComponents,
+                                                   view: PassportOrIdCardDetailsView
+                                                 )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
+
+  private val form: Form[CombinedPassportOrIdCard] = formProvider.withPrefix("livingSettlor.passportOrIdCardDetails")
+
+  def onPageLoad(mode: Mode): Action[AnyContent] = (standardActionSets.verifiedForUtr andThen nameAction) {
+    implicit request =>
+
+      val preparedForm = request.userAnswers.get(PassportOrIdCardDetailsPage) match {
+        case None => form
+        case Some(value) => form.fill(value)
+      }
+
+      Ok(view(preparedForm, request.settlorName, countryOptions.options, mode))
+  }
+
+  def onSubmit(mode: Mode): Action[AnyContent] = (standardActionSets.verifiedForUtr andThen nameAction).async {
+    implicit request =>
+
+      form.bindFromRequest().fold(
+        formWithErrors =>
+          Future.successful(BadRequest(view(formWithErrors, request.settlorName, countryOptions.options, mode))),
+
+        value =>
+          for {
+            updatedAnswers <- Future.fromTry(request.userAnswers.set(PassportOrIdCardDetailsPage, value))
+            _              <- playbackRepository.set(updatedAnswers)
+          } yield Redirect(navigator.nextPage(PassportOrIdCardDetailsPage, updatedAnswers))
+      )
+  }
+}
---
+++ test/views/individual/living/amend/PassportOrIdCardDetailsViewSpec.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.individual.living.amend
+
+import base.FakeTrustsApp
+import controllers.individual.living.amend.routes
+import forms.CombinedPassportOrIdCardDetailsFormProvider
+import models.{CombinedPassportOrIdCard, Mode, Name, NormalMode}
+import play.api.data.Form
+import play.twirl.api.HtmlFormat
+import utils.InputOption
+import utils.countryOptions.CountryOptions
+import views.behaviours.QuestionViewBehaviours
+import views.html.individual.living.amend.PassportOrIdCardDetailsView
+
+class PassportOrIdCardDetailsViewSpec extends QuestionViewBehaviours[CombinedPassportOrIdCard] with FakeTrustsApp {
+
+  private val messageKeyPrefix: String = "livingSettlor.passportOrIdCardDetails"
+  private val name: Name = Name("First", Some("Middle"), "Last")
+
+  private val mode: Mode = NormalMode
+
+  override val form: Form[CombinedPassportOrIdCard] = new CombinedPassportOrIdCardDetailsFormProvider(frontendAppConfig).withPrefix(messageKeyPrefix)
+
+  "PassportOrIdCardDetails View" must {
+
+    val view = viewFor[PassportOrIdCardDetailsView](Some(emptyUserAnswers))
+
+    val countryOptions: Seq[InputOption] = app.injector.instanceOf[CountryOptions].options
+
+    def applyView(form: Form[_]): HtmlFormat.Appendable =
+      view.apply(form, name.displayName, countryOptions, mode)(fakeRequest, messages)
+
+    behave like dynamicTitlePage(applyView(form), messageKeyPrefix, name.displayName)
+
+    behave like pageWithBackLink(applyView(form))
+
+    "fields" must {
+
+      behave like pageWithPassportOrIDCardDetailsFields(
+        form,
+        applyView,
+        messageKeyPrefix,
+        routes.PassportOrIdCardDetailsController.onSubmit(mode).url,
+        Seq(("country", None), ("number", None)),
+        "expiryDate",
+        name.displayName
+      )
+    }
+
+    behave like pageWithASubmitButton(applyView(form))
+  }
+}
